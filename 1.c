936d195bdf2cba907f1a1f8faeea753b56352674
S498 <35898371+S498@users.noreply.github.com>
2018-04-08 17:08:51 +0530

    Update 1.c

--- 1.c
+++ 1.c
@@ -2,11 +2,25 @@
 #include<pthread.h>
 #include<unistd.h>
 #include<stdlib.h>
+#include<semaphore.h>
 pthread_t *Students;
 pthread_t TA;
 int chair=0;
 int curr=0;
-void *TA_activity();
+pthread_mutex_t ChairAccess;
+sem_t TA_sleep,student_sem,chair_sem[3];
+void *TA_activity()
+{
+  while(true)
+  {
+    pthread_mutex)lock(&chairAccess);
+    if(chair==0)
+    {
+      pthread_mutex_unlock(&chairAccess);
+      break;
+    }
+    //Continue from if other than 0 chair available.
+}
 void *Student_activity(void *threadID);
 main()
 {
